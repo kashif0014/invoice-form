beb5b025f8c391483e4ad62547d74819c0f023b3
kashif <51692352+kashif0014@users.noreply.github.com>
2025-06-11 13:43:56 +0530

    fix: update route to login

--- src/routers/Routes.tsx
+++ src/routers/Routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { Invoice } from "../pages/Invoice";
 import { Invoice_Form_Route, Login_Page_Route } from "../constants";
@@ -6,8 +6,11 @@ import { Invoice_Form_Route, Login_Page_Route } from "../constants";
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to={Login_Page_Route} />} />
+
       <Route path={Login_Page_Route} element={<Login />} />
       <Route path={Invoice_Form_Route} element={<Invoice />} />
+
       <Route path="*" element={<Login />} />
     </Routes>
   );
